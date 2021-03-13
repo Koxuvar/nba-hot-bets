4e970f33d9c543d0673ae37be79e629bdca28d39
MMVHome <connortsullivan1@gmail.com>
2021-03-12 20:10:40 -0600

    added workflow for basic index.js functionality

--- js-pseudo.js
+++ js-pseudo.js
@@ -1,1 +1,49 @@
 // pseudo code goes here
+
+// --------------------------------------- OBJECTIVES ---------------------------------------//
+
+/*
+   
+    -get games - function getGames()
+    -create object per game
+    -game object example - 
+        game = {
+            team1: {
+                name:"teamName",
+                score:81,
+                logo:"./assets/images/logo"
+            },
+            team2: {
+                name:"teamName",
+                score:81,
+                logo:"./assets/images/logo"
+            },
+            gameDate:"March 15. 2021",
+            ...
+
+        }
+    -display games
+    -on click of game:
+        get odds for that game
+        serve those to new page - location
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
